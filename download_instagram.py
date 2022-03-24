c2908ae4bce4a2075b29190bd26260d68ef0143a
Vincent Lara <lara.vincent@gmail.com>
2022-03-24 18:16:08 +0100

    Fix donwload_instagram

--- download_instagram.py
+++ download_instagram.py
@@ -1,5 +1,5 @@
 from instagrapi import Client
-import os
+import os, time, random
 
 ACCOUNT_USERNAME = os.getenv('INSTAGRAM_USERNAME')
 ACCOUNT_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
@@ -7,8 +7,12 @@ ACCOUNT_PASSWORD = os.getenv('INSTAGRAM_PASSWORD')
 cl = Client()
 cl.login(ACCOUNT_USERNAME, ACCOUNT_PASSWORD)
 user_id = cl.user_id_from_username("bain.de.midi")
+time.sleep(random.randrange(890, 2500)/1000)
 medias = cl.user_medias(user_id)
 photos = [media for media in medias if media.media_type == 1]
 
+time.sleep(random.randrange(890, 2500)/1000)
+path = "downloads/photos/instagram"
+os.makedirs(path, exist_ok=True)
 for photo in photos:
-	cl.photo_download(photo.pk)
+	cl.photo_download(photo.pk, path)
